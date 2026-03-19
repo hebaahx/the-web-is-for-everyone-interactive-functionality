0bfd743f5cb0402200da0c086fb6c7d34c06e6b9
handouh1 <heba.handouka@hva.nl>
2026-03-20 00:42:01 +0100

    #4 JS

--- public/script.js
+++ public/script.js
@@ -1,5 +1,11 @@
 // Stap 1: zoek de elementen op in de pagina
+const comments = document.getElementById('comments')
+const popover = document.getElementById('reactie-popover')
 
 // Stap 2: luister naar een klik op de knop
-
-// Stap 3: zet de popover aan of uit
+comments.addEventListener('click', function() {
+    
+    // Stap 3: zet de popover aan of uit
+    popover.hidden = !popover.hidden
+ 
+})
